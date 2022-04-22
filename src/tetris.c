9249bfa3e71f03b549cb7251517652c22439eb33
Bryce Berger <bryce.z.berger@gmail.com>
2022-04-22 06:20:04 -0400

    Add pausing to tetris

--- src/tetris.c
+++ src/tetris.c
@@ -29,6 +29,8 @@ const u8 pieces[] = {
     0170, // I
 };
 
+#define FRAMERATE 30
+
 // frames between fall
 #define STARTING_RATE 15
 int rate = STARTING_RATE;
@@ -37,14 +39,21 @@ bool fast_fall = false;
 int score = 0;
 volatile int lose = 0;
 
-// fills the screen with white
-void fill_white() {
-    for (int x = 0; x < 240; x += 16)
-        for (int y = 0; y < 320; y += 16)
+// fills the screen with white, adds black border
+void init_background() {
+    for (int y = 0; y < 320; y += 16) {
+
+        for (int x = 0; x < 240; x += 16)
             gpu_buffer_add(x, y, white, 0);
+
+        gpu_buffer_add(0, y, colors[0], 0);
+        gpu_buffer_add(16, y, colors[0], 0);
+        gpu_buffer_add(208, y, colors[0], 0);
+        gpu_buffer_add(224, y, colors[0], 0);
+    }
 }
 
-void print_to_screen(const char* fstring, u32 number) {
+void print_to_screen(const char *fstring, u32 number) {
     set_lcd_flag(TEXT_SENDING);
     while (!check_lcd_flag(GPU_DISABLE))
         ;
@@ -64,14 +73,9 @@ void draw_background() {
             // 3 bits of color information per tile
             gpu_buffer_add((200 - 16) - j * 16, ypos, colors[(board[i] >> (j * 3)) & 7], 0);
         }
-        gpu_buffer_add(0, ypos, colors[0], 0);
-        gpu_buffer_add(16, ypos, colors[0], 0);
-        gpu_buffer_add(208, ypos, colors[0], 0);
-        gpu_buffer_add(224, ypos, colors[0], 0);
     }
-    draw_next_piece();
 
-    print_to_screen("\033[0;0H\t\t\t\t\t\tCleared: %d\n", score);
+    print_to_screen("\033[48;2;0;0;0m\033[0;28H Cleared: %d         \n", score);
 }
 
 // handle clearing rows
@@ -88,6 +92,7 @@ void update_background() {
                 break;
             }
         }
+
         if (clear) {
             // row was full, clear it and move others down
             for (int k = i; k > 0; k--) {
@@ -95,6 +100,7 @@ void update_background() {
             }
             board[0] = 0;
             i++; // check the row that we just cleared again, stuff was moved down
+
             // update score
             score++;
             updated = 1;
@@ -288,17 +294,20 @@ void lower_piece() {
     handle_rotation(check_valid_move);
     if (invalid_move) {
         invalid_move = 0;
+
         current_piece.y--;
         if (current_piece.y == 0) {
             lose = 1;
             unhook_timer();
         }
+
         handle_rotation(add_to_background);
+
         erase_next_piece();
         get_new_piece();
-        draw_next_piece();
+
         update_background();
-        // draw_background();
+        draw_next_piece();
     } else {
         current_piece.y--;
         handle_rotation(erase_piece);
@@ -307,11 +316,21 @@ void lower_piece() {
     }
 }
 
+void draw_frame(void);
+
+void tetris_paused(void) {
+    const KeyEvent *event;
+    while ((event = get_keyboard_event())) {
+        if (event->class == PAUSE_KEY) {
+            hook_timer(FRAMERATE, draw_frame);
+        }
+    }
+}
+
 void draw_frame(void) {
     static int state = 0; // frames since last fall
 
-    // u32 starttime = TIM2->CNT;
-    // print_to_screen("\033[1;0H First: %u\n", starttime);
+    u32 starttime = TIM2->CNT;
 
     const KeyEvent *event;
     while ((event = get_keyboard_event())) {
@@ -325,32 +344,39 @@ void draw_frame(void) {
             move_current_piece(-1);
         else if (event->class == UP_ARROW_KEY)
             rotate_current_piece();
+        else if (event->class == PAUSE_KEY) {
+            hook_timer(5, tetris_paused);
+        }
     }
 
     state += fast_fall ? 3 : 1;
     if (state >= rate) {
         state = 0;
         lower_piece();
-        // print_to_screen("\033[1;0HDiff: %u     \n", starttime - TIM2->CNT);
-        // print_to_screen("\033[2;0H   %%: %.4f     \n", 100 * (float) (starttime - TIM2->CNT) / TIM2->ARR);
-        // // print_to_screen("\033[3;0H   %%: %u     \n", TIM2->ARR);
+        print_to_screen("\033[1;0HDiff: %u     \n", starttime - TIM2->CNT);
+        print_to_screen("\033[2;0H   %%: %.4f     \n", ((float) (100 * (starttime - TIM2->CNT))) / TIM2->ARR);
+        // print_to_screen("\033[3;0H   %%: %u     \n", TIM2->ARR);
     }
 }
 
 int run_tetris() {
     init_gpu();
-    fill_white();
+    init_background();
+    draw_background();
+
     configure_keyboard();
-    draw_background();
     int i = 0;
-    while (!get_keyboard_event()) {
+    while (!get_keyboard_event())
         i++;
-    }
     mix_random(i);
+
     get_new_piece();
-    draw_background();
-    hook_timer(30, draw_frame);
+    draw_next_piece();
+
+    hook_timer(FRAMERATE, draw_frame);
+
     while (!lose)
-        ;
+        asm volatile("wfi");
+
     return score;
 }
